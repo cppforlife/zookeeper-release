b4344476ae9eddbdfcdaf978036f284c5d10a3d7
dmitriy kalinin <dmitriy@pivotallabs.com>
2017-06-06 00:40:00 -0700

    dasherize job and packages

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Uses BOSH [links](https://bosh.io/docs/links.html) as an example.
 # Useful commands
 
 ```
-$ bosh -d zookeeper run-errand smoke_tests
+$ bosh -d zookeeper run-errand smoke-tests
 $ bosh -d zookeeper ssh -c '/var/vcap/jobs/zookeeper/bin/ctl status' -r
 ```
---
+++ tests/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e # -x
+
+echo "-----> `date`: Upload stemcell"
+bosh -n upload-stemcell "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=3421.4" \
+  --sha1 e7c440fc20bb4bea302d4bfdc2369367d1a3666e
+
+echo "-----> `date`: Delete previous deployment"
+bosh -n -d zookeeper delete-deployment --force
+
+echo "-----> `date`: Deploy"
+( set -e; cd ./..; bosh -n -d zookeeper deploy ./manifests/zookeeper.yml -o ./manifests/dev.yml )
+
+echo "-----> `date`: Recreate all VMs"
+bosh -n -d zookeeper recreate
+
+echo "-----> `date`: Exercise deployment"
+bosh -n -d zookeeper run-errand smoke-tests
+
+echo "-----> `date`: Restart deployment"
+bosh -n -d zookeeper restart
+
+echo "-----> `date`: Report any problems"
+bosh -n -d zookeeper cck --report
+
+echo "-----> `date`: Delete random VM"
+bosh -n -d zookeeper delete-vm `bosh -d zookeeper vms|sort|cut -f5|head -1`
+
+echo "-----> `date`: Fix deleted VM"
+bosh -n -d zookeeper cck --auto
+
+echo "-----> `date`: Delete deployment"
+bosh -n -d zookeeper delete-deployment
+
+echo "-----> `date`: Clean up disks, etc."
+bosh -n -d zookeeper clean-up --all
+
+echo "-----> `date`: Done"
