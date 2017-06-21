ae84a47267010d8b5beef68b416340abc06b3a59
dmitriy kalinin <dmitriy@pivotallabs.com>
2017-06-21 05:44:28 -0700

    skip stemcell upload if already present

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,9 @@ set -e # -x
 
 echo "-----> `date`: Upload stemcell"
 bosh -n upload-stemcell "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=3421.4" \
-  --sha1 e7c440fc20bb4bea302d4bfdc2369367d1a3666e
+  --sha1 e7c440fc20bb4bea302d4bfdc2369367d1a3666e \
+  --name bosh-warden-boshlite-ubuntu-trusty-go_agent \
+  --version 3421.4
 
 echo "-----> `date`: Delete previous deployment"
 bosh -n -d zookeeper delete-deployment --force
